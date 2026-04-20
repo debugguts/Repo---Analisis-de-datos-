4a05b066c194d7a1fa12cd63003458ed4cad8031
Gustavo Sinanis <gutts11ao@gmail.com>
2026-04-20 15:10:42 -0300

    Agrega script de analisis estadistico

---
+++ analisis.py
@@ -0,0 +1,9 @@
+# Mi primer script versionado con Git
+import statistics
+
+datos = [10, 20, 30, 40, 50]
+
+print("--- Analisis de datos ---")
+print("Promedio:", statistics.mean(datos))
+print("Mediana:", statistics.median(datos))
+print("Desv. estandar:", round(statistics.stdev(datos), 2))
